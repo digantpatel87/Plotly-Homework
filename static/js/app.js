8aa897ed73f5e8022e0b020cdfe19277d60f02a5
digantpatel87 <80235083+digantpatel87@users.noreply.github.com>
2021-06-28 17:50:26 -0400

    Final

--- static/js/app.js
+++ static/js/app.js
@@ -14,19 +14,37 @@ function init() {
             selDataset.appendChild(opt);
         };
 
-        var initial_value = "940";
+        var dropdownMenu = d3.select("#selDataset");
+        // Assign the value of the dropdown menu option to a variable
+        var selectedValue = dropdownMenu.property("value");
+
+        var initial_value = selectedValue;
+        var initial_intvalue = selectedValue;
 
         function filterSamples(InitialData) {
             return InitialData.id == initial_value;
         }
 
+        function filtermetadata(InitialintData) {
+            return InitialintData.id == initial_intvalue;
+        }
+
         // 2. Use filter() to pass the function as its argument
         var filteredsamples = data.samples.filter(filterSamples);
+        var filteredmetaData = data.metadata.filter(filtermetadata);
 
         // 3. Use the map method 
         var otu_ids = filteredsamples.map(samples => samples.otu_ids);
         var sample_values = filteredsamples.map(samplesm => samplesm.sample_values);
         var otu_labels = filteredsamples.map(samplest => samplest.otu_labels);
+
+        var ethnicity = filteredmetaData.map(metadata => metadata.ethnicity);
+        var age = filteredmetaData.map(metadata => metadata.age);
+        var gender = filteredmetaData.map(metadata => metadata.gender);
+        var location = filteredmetaData.map(metadata => metadata.location);
+        var bbtype = filteredmetaData.map(metadata => metadata.bbtype);
+        var wfreq = filteredmetaData.map(metadata => metadata.wfreq);
+
 
         // Slice the first 10 objects for plotting
         var Top10otu_ids = otu_ids[0].slice(0, 10);
@@ -75,9 +93,59 @@ function init() {
           
           Plotly.newPlot('bubble', data, layout);
 
+          var metadatadiv = document.getElementById("sample-metadata");
+          var metadatatable = document.createElement("table");
+          var row = document.createElement("tr");
+          var column = document.createElement("td");
+
+          metadatadiv.innerHTML = "";
+          column.innerHTML = "id: " + initial_value;
+          row.appendChild(column);
+          metadatatable.appendChild(row);
+
+        row = document.createElement("tr");
+        column = document.createElement("td");
+        column.innerHTML = "ethnicity: " + ethnicity;
+        row.appendChild(column);
+        metadatatable.appendChild(row);
+
+        row = document.createElement("tr");
+        column = document.createElement("td");
+        column.innerHTML = "gender: " + gender;
+        row.appendChild(column);
+        metadatatable.appendChild(row);
+
+        row = document.createElement("tr");
+        column = document.createElement("td");
+        column.innerHTML = "age: " + age;
+        row.appendChild(column);
+        metadatatable.appendChild(row);
+
+        row = document.createElement("tr");
+        column = document.createElement("td");
+        column.innerHTML = "location: " + location;
+        row.appendChild(column);
+        metadatatable.appendChild(row);
+
+        row = document.createElement("tr");
+        column = document.createElement("td");
+        column.innerHTML = "bbtype: " + bbtype;
+        row.appendChild(column);
+        metadatatable.appendChild(row);
+
+        row = document.createElement("tr");
+        column = document.createElement("td");
+        column.innerHTML = "wfreq: " + wfreq;
+        row.appendChild(column);
+        metadatatable.appendChild(row);
+
+          metadatadiv.appendChild(metadatatable);
 
     });
 }
+
+d3.selectAll("#selDataset").on("change", init);
+
 // Initializes the page with a default plot
 init();
 
